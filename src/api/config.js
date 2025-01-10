ac1b86d8aa7e56798006c075290429fa8ce9277b
Ehtisham Ul Haq <125582602+EhtishamUlHaq265@users.noreply.github.com>
2025-01-11 00:16:57 +0500

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,3 @@
-export const API_BASE_URL_AUTH = 'http://49.13.199.0:4000/api/v1/auth/';
-export const API_BASE_URL_COMPANY = 'http://49.13.199.0:4000/api/v1/auth/';
-export const API_BASE_URL_ADMIN = 'http://49.13.199.0:4000/api/v1/admin/';
-
-// export const API_BASE_URL_AUTH = 'http://192.168.178.32:4500/api/v1/auth/';
-// export const API_BASE_URL_COMPANY = 'http://192.168.178.32:4500/api/v1/auth/';
-// export const API_BASE_URL_ADMIN = 'http://192.168.178.32:4500/api/v1/admin/';
+export const API_BASE_URL_AUTH = "URL";
+export const API_BASE_URL_COMPANY = 'URL';
+export const API_BASE_URL_ADMIN = 'URL';
